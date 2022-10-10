23fb0242a82ed6fd3e40f225fff4d8ca073590e6
Stalin Nutulapati <110710758+nstalin1621@users.noreply.github.com>
2022-10-10 13:55:32 +0530

    Add files via upload

--- swap.cpp
+++ swap.cpp
@@ -1,17 +1,19 @@
-// C++ program for illustration of swap() function
-#include <bits/stdc++.h>
-using namespace std;
-  
-int main()
+#include<iostream>
+using namespace std;  
+void swap(int *x, int *y)
 {
-    int a = 10;
-    int b = 20;
-    cout << "Value of a before: " << a << endl;
-    cout << "Value of b before: " << b << endl;
-  
-    // swap values of the variables
-    swap(a, b);
-    cout << "Value of a now: " << a << endl;
-    cout << "Value of b now: " << b << endl;
-    return 0;
-}	
+	int swap;
+	swap=*x;
+	*x=*y;
+	*y=swap;
+}
+int main() 
+{  
+	int x=500, y=100;  
+	swap(&x, &y);  // passing value to function
+	cout<<"Value of x is: "<<x<<endl;
+	cout<<"Value of y is: "<<y<<endl;
+	return 0;
+}  
+
+
